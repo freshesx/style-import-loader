9e5ad4d9a51699dd2f88958dfcaac91d47aa9f6a
ChaselWu <minowu@foxmail.com>
2017-11-24 17:27:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const path = require('path')
+const utils = require('loader-utils')
 
 module.exports = source => {
-  const styleFile = 'dist/main.css'
-  const scopeName = '@humans/[\\w-]+'
+  const options = utils.getOptions(this) || {}
+  const styleFile = options.style || 'dist/main.css'
+  const scopeName = options.scoped || '@humans/[\\w-]+'
   const lineReg = new RegExp(`^[\\s]*import[\\w\\s,{}]+'${scopeName}'$`, 'gm')
   const pkgReg = new RegExp(`${scopeName}`, 'g')
   const matched = source.match(lineReg)
 
-  matched.forEach(line => {
-    const pkg = line.match(pkgReg)[0]
-    const style = `import '${pkg}/${styleFile}'`
-    source = source.replace(line, '$&\n  ' + style)
-  })
+  if (matched.length > 0) {
+    matched.forEach(line => {
+      const pkg = line.match(pkgReg)[0]
+      const style = `import '${pkg}/${styleFile}'`
+      source = source.replace(line, '$&\n  ' + style)
+    })
+  }
 
   return source
 }
